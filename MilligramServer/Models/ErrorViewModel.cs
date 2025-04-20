74723eae5403bfb65d0d26031bfa81914c71bfea
SteelPhill <shnaider03021993@gmail.com>
2025-04-20 22:32:10 +0400

    Initialization

---
+++ MilligramServer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MilligramServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
