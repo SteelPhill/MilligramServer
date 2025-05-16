b723f3f183c819b608f8c4f69c78ca9a5b2c5f96
SteelPhill <shnaider03021993@gmail.com>
2025-05-16 11:25:32 +0400

    Add: ChangeOnDeleteInTables migration

---
+++ MilligramServer.Database/Migrations/20250516072116_ChangeOnDeleteInTables.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilligramServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOnDeleteInTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_OwnerUserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersChats_Chats_ChatId",
+                table: "UsersChats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersChats_Users_UserId",
+                table: "UsersChats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersRoles_Roles_RoleId",
+                table: "UsersRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersRoles_Users_UserId",
+                table: "UsersRoles");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_OwnerUserId",
+                table: "Contacts",
+                column: "OwnerUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersChats_Chats_ChatId",
+                table: "UsersChats",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersChats_Users_UserId",
+                table: "UsersChats",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersRoles_Roles_RoleId",
+                table: "UsersRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersRoles_Users_UserId",
+                table: "UsersRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_OwnerUserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersChats_Chats_ChatId",
+                table: "UsersChats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersChats_Users_UserId",
+                table: "UsersChats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersRoles_Roles_RoleId",
+                table: "UsersRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersRoles_Users_UserId",
+                table: "UsersRoles");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_OwnerUserId",
+                table: "Contacts",
+                column: "OwnerUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersChats_Chats_ChatId",
+                table: "UsersChats",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersChats_Users_UserId",
+                table: "UsersChats",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersRoles_Roles_RoleId",
+                table: "UsersRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersRoles_Users_UserId",
+                table: "UsersRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
