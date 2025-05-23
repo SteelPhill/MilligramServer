f1e9bee4979c6e202185d65b8ac005eee773672e
SteelPhill <shnaider03021993@gmail.com>
2025-05-23 14:20:05 +0400

    Add: Api Exceptions

---
+++ MilligramServer/Exceptions/ApiExceptionBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace MilligramServer.Exceptions;
+
+public abstract class ApiExceptionBase : Exception
+{
+    public abstract HttpStatusCode Code { get; }
+
+    protected ApiExceptionBase(string? message, Exception? exception)
+        : base(message ?? string.Empty, exception)
+    {
+    }
+}
